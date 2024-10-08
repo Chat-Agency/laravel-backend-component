95914f4aa829a3d91fa4c91bc4db77d643263fe5
Victorchat <victor@chatagency.com>
2024-10-08 15:02:43 -0400

    Small design change

--- resources/views/_themes/tailwind/table.blade.php
+++ resources/views/_themes/tailwind/table.blade.php
@@ -3,6 +3,6 @@
 return [
     'container' => 'inline-block min-w-full shadow rounded-lg overflow-hidden',
     'table' => 'min-w-full',
-    'th' => 'px-3 py-3 border-b-2 border-gray-300 bg-gray-100 dark:border-slate-900 dark:bg-slate-900 text-left text-xs font-bold uppercase tracking-wider',
+    'th' => 'px-3 py-3 border-b-2 border-gray-300 bg-gray-200 dark:border-slate-900 dark:bg-slate-900 text-left text-xs font-bold uppercase tracking-wider',
     'td' => 'px-3 py-3 border-b border-gray-300 bg-white dark:bg-slate-800 align-middle',
 ];
