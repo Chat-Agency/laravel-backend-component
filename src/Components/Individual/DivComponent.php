0dd7d5ddcbc9dde4ccaf37115097fd47f5cb97fd
Victorchat <victor@chatagency.com>
2025-01-24 15:37:18 -0400

    Fix individual components return types. Considering using generics

--- src/Components/Individual/DivComponent.php
+++ src/Components/Individual/DivComponent.php
@@ -5,12 +5,12 @@ declare(strict_types=1);
 namespace ChatAgency\BackendComponents\Components\Individual;
 
 use ChatAgency\BackendComponents\Components\DefaultAttributeBag;
-use ChatAgency\BackendComponents\Concerns\HasContent;
+use ChatAgency\BackendComponents\Components\DefaultContentsComponent;
 use ChatAgency\BackendComponents\Concerns\IsBackendComponent;
 use ChatAgency\BackendComponents\Concerns\IsThemeable;
 use ChatAgency\BackendComponents\Contracts\AttributeBag;
 use ChatAgency\BackendComponents\Contracts\BackendComponent;
-use ChatAgency\BackendComponents\Contracts\ContentComponent;
+use ChatAgency\BackendComponents\Contracts\ContentsComponent;
 use ChatAgency\BackendComponents\Contracts\ThemeComponent;
 use ChatAgency\BackendComponents\Contracts\ThemeManager;
 use ChatAgency\BackendComponents\Enums\ComponentEnum;
@@ -20,20 +20,16 @@ use Illuminate\Contracts\Support\Htmlable;
 
 use function ChatAgency\BackendComponents\backendComponentNamespace;
 
-class DivComponent implements Arrayable, BackendComponent, ContentComponent, Htmlable, ThemeComponent
+class DivComponent implements Arrayable, BackendComponent, Htmlable, ThemeComponent
 {
-    use HasContent,
-        IsBackendComponent,
+    use IsBackendComponent,
         IsThemeable;
+
+    private array $content = [];
 
     public function __construct(
         private ThemeManager $themeManager = new DefaultThemeManager
     ) {}
-
-    public function getContent($key = null): string|Arrayable|BackendComponent|ContentComponent|Htmlable|ThemeComponent|null
-    {
-        return $this->content[$key] ?? null;
-    }
 
     public function toArray(): array
     {
@@ -78,5 +74,42 @@ class DivComponent implements Arrayable, BackendComponent, ContentComponent, Htm
                 $this->compileTheme(),
             ))
             ->render();
+    }
+
+    public function getContent($key = null): string|DivComponent|null
+    {
+        return $this->content[$key] ?? null;
+    }
+
+    public function getContents(): array
+    {
+        return $this->content;
+    }
+
+    public function setContent(string|BackendComponent $content, $key = null): static
+    {
+        if ($key) {
+            $this->content[$key] = $content;
+
+            return $this;
+        }
+
+        $this->content[] = $content;
+
+        return $this;
+    }
+
+    public function setContents(array $contents): static
+    {
+        foreach ($contents as $key => $content) {
+            $this->setContent($content, $key);
+        }
+
+        return $this;
+    }
+
+    public function processContent(): ContentsComponent
+    {
+        return new DefaultContentsComponent($this->getContents());
     }
 }
