4bd6e7e551e5e68672132c3a80144a62ffac45e5
Victorchat <victor@chatagency.com>
2025-06-06 16:37:41 -0400

    Add border radius theme

---
+++ resources/views/_themes/tailwind/border-radius.blade.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'default' => 'rounded',
+    'xs' => 'rounded-xs',
+    'sm' => 'rounded-sm',
+    'md' => 'rounded-md',
+    'lg' => 'rounded-lg',
+    'xl' => 'rounded-xl',
+    '2xl' => 'rounded-2xl',
+    '3xl' => 'rounded-3xl',
+    '4xl' => 'rounded-4xl',
+    'none' => 'rounded-none',
+    'full' => 'rounded-full',
+];
+
