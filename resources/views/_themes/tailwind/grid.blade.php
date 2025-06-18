fb007ae55a14144f662130f48a1f176457f6d717
Victorchat <victor@chatagency.com>
2025-06-18 17:44:30 -0400

    Add missing theme files

---
+++ resources/views/_themes/tailwind/grid.blade.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+    
+    'row-auto'=> 'row-auto',
+
+    'cols-1' => 'grid-cols-1',
+    'cols-2' => 'grid-cols-2',
+    'cols-3' => 'grid-cols-3',
+    'cols-4' => 'grid-cols-4',
+
+    'rows-1' => 'grid-rows-1',
+    'rows-2' => 'grid-rows-2',
+    'rows-3' => 'grid-rows-3',
+    'rows-4' => 'grid-rows-4',
+
+    'gap-sm' => 'gap-1',
+    'gap-md' => 'gap-3',
+    'gap-lg' => 'gap-6',
+    
+    'col' => 'flex-col',
+    'row' => 'flex-row',
+    'col-reverse' => 'flex-col-reverse',
+    'row-reverse' => 'flex-row-reverse',
+    
+    'items-center' => 'items-center',
+    'items-start' => 'items-start',
+    'items-end' => 'items-end',
+    'items-baseline' => 'items-baseline',
+    'items-stretch' => 'items-stretch',
+
+    'justify-items-start' => 'justify-items-start',
+    'justify-items-end' => 'justify-items-end',
+    'justify-items-center' => 'justify-items-center',
+    'justify-items-stretch' => 'justify-items-stretch',
+
+    'wrap' => 'flex-wrap',
+    'mobile-wrap' => 'flex-wrap md:flex-nowrap',
+];
