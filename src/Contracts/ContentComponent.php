dfa5eb75ef9ced4c73c4b794cc6fab73b60f97b0
Victorchat <victor@chatagency.com>
2025-07-18 21:03:42 -0400

    Update interface

--- src/Contracts/ContentComponent.php
+++ src/Contracts/ContentComponent.php
@@ -6,17 +6,17 @@ namespace ChatAgency\BackendComponents\Contracts;
 
 interface ContentComponent
 {
-    public function getContent(string|int $key): CompoundComponent|int|string|null;
+    public function getContent(string|int $key): CompoundComponent|BackendComponent|int|string|null;
 
     /**
-     * @return array<string|int, string|int|CompoundComponent>
+     * @return array<string|int, string|int|CompoundComponent|BackendComponent>
      */
     public function getContents(): array;
 
-    public function setContent(string|CompoundComponent $content, string|int|null $key = null): static;
+    public function setContent(int|string|CompoundComponent|BackendComponent $content, string|int|null $key = null): static;
 
     /**
-     * @param  array<string|int, string|int|CompoundComponent>  $contents
+     * @param  array<string|int, string|int|CompoundComponent|BackendComponent>  $contents
      */
     public function setContents(array $contents): static;
 
