83edcbf5e9f209ef4c539b5d659d56728273198a
Victorchat <victor@chatagency.com>
2025-12-03 11:39:49 -0400

    refactor: refactor views into regular and livewire components

--- resources/views/_utilities/resolve-component.blade.php
+++ resources/views/_utilities/resolve-component.blade.php
@@ -1,23 +1,6 @@
 @php
-    /** @var \ChatAgency\BackendComponents\Contracts\LaravelBackendComponent $component */
+    /** @var \ChatAgency\BackendComponents\Contracts\CompoundComponent $component */
+    $path = $component->getComponentPath();
+    $componentArray =  $component->getAttributeBag();
 @endphp
-
-@if($component->isLivewire())
-    @php
-        $name = $component->getName();
-        $params = $component->getLivewireParams();
-        $key = $component->getLivewireKey();
-    @endphp
-    
-    @if ($key)
-        @livewire($name, $params, key($key))
-    @else 
-        @livewire($name, $params)
-    @endif 
-@else
-    @php
-        $path = $component->getComponentPath();
-        $componentArray =  $component->getAttributeBag();
-    @endphp
-    <x-dynamic-component :component="$path" :attrs="$componentArray" />
-@endif 
+<x-dynamic-component :component="$path" :attrs="$componentArray" />
---
+++ resources/views/_utilities/resolve-livewire-component.php
@@ -0,0 +1,12 @@
+@php
+    /** @var \ChatAgency\BackendComponents\Contracts\CompoundComponent $component */
+    $name = $component->getName();
+    $params = $component->getLivewireParams();
+    $key = $component->getLivewireKey();
+@endphp
+
+@if ($key)
+    @livewire($name, $params, key($key))
+@else 
+    @livewire($name, $params)
+@endif 
